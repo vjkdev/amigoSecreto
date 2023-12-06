08addaee618a740889dfd215cb656135430bc433
Vanderlei Jose Kempf <vandercurso@gmail.com>
2023-12-06 16:33:15 -0300

    Create project and Server

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import https from 'https';
+import http from 'http';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const runServer = (port: number, server: http.Server) => {
+    server.listen(port, () => {
+        console.log(`🚀Running at PORT: ${port}`);
+    });
+}   
+
+const regularServer = http.createServer(app);
+if(process.env.NODE_ENV === 'production') {
+    // TODO: configurar SSL
+    // TODO: rodar server na 80 e na 443
+} else {
+    const serverPort: number = process.env.PORT ? parseInt(process.env.PORT) : 9000;
+    runServer(serverPort, regularServer);
+}
+
+
+
+/*https://www.youtube.com/watch?v=mKa1MuB1HMk 1:02:19*/
+
